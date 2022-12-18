3fae67ca9d5a444c863e651d32269c584131df26
Ajay Jakate <ajay.jakate@gmail.com>
2022-12-18 02:35:15 -0600

    add speed factor

--- run.clj
+++ run.clj
@@ -1,14 +1,24 @@
-(require '[babashka.process :refer [process check sh pipeline pb]])
+(require '[babashka.process :refer [sh]])
 
 (def input-file "inputs/test_new.mov")
 
-;; TODO: implement
-(def inc-fact 1.15)
+(def inc-fact 2.0)
+
+(defn print-round [num, prec]
+  (apply str (take prec (str num))))
+
+(defn get-speeds-for-index [idx]
+  (let [raw-speed (reduce * (repeat (+ 1 idx) (float inc-fact)))
+        inverse (/ 1 raw-speed)]
+    {:audio (print-round raw-speed 6)
+     :video (print-round inverse 6)}))
 
 (def timestamps
   ["00:08.500"
    "00:13.750"
    "00:17.500"])
+
+(def num-segments (+ (count timestamps) 1))
 
 (defn safely [stringg]
   (let [chary (seq (char-array stringg))]
@@ -24,7 +34,7 @@
 (def command-start (str "ffmpeg -y -i " input-file " -filter_complex "))
 
 (def first-segment
-  (str "[0:v]trim=" firstone 
+  (str "[0:v]trim=" firstone
        ",setpts=(PTS-STARTPTS)[sv];[0:a]atrim="
        firstone
        ",asetpts=(PTS-STARTPTS)[sa];"))
@@ -36,24 +46,20 @@
    (fn [idx stamps]
      (let [start (safely (first stamps))
            end (safely (second stamps))
-           ]
-       ;; TODO: fix le SPEED
-       (str "[0:v]trim=start='" start "':end='" end "',setpts=0.50*(PTS-STARTPTS)[v" idx "];"
-            "[0:a]atrim=start='" start "':end='" end "',asetpts=(PTS-STARTPTS),atempo=2.00[a" idx "];"
-            )))
+           speeds (get-speeds-for-index idx)]
+       (str "[0:v]trim=start='" start "':end='" end "',setpts=" (:video speeds) "*(PTS-STARTPTS)[v" idx "];"
+            "[0:a]atrim=start='" start "':end='" end "',asetpts=(PTS-STARTPTS),atempo=" (:audio speeds) "[a" idx "];")))
    iterator))
 
 (def last-segment
-  (str "[0:v]trim=start='" (safely (last timestamps)) "',setpts=0.25*(PTS-STARTPTS)[ev];"
-       "[0:a]atrim=start='" (safely (last timestamps)) "',asetpts=(PTS-STARTPTS),atempo=4.00[ea];"))
+  (let [speeds (get-speeds-for-index (- num-segments 1))] 
+    (str "[0:v]trim=start='" (safely (last timestamps)) "',setpts=" (:video speeds) "*(PTS-STARTPTS)[ev];"
+         "[0:a]atrim=start='" (safely (last timestamps)) "',asetpts=(PTS-STARTPTS),atempo=" (:audio speeds) "[ea];")))
 
 (def last-line
   (str "[sv][sa]"
-       
        (apply str (map-indexed (fn [idx _] (str "[v" idx "]" "[a" idx "]")) iterator))
-       
-       "[ev][ea]concat=n=4:v=1:a=1"))
-
+       "[ev][ea]concat=n=" num-segments ":v=1:a=1"))
 
 (def final
   (str command-start "\"" first-segment (apply str chunkss) last-segment last-line "\"" " outputs/out.mov"))
