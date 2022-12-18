ea37a1ddb4eb72f9563c0f9d3dd2584792891e00
Ajay Jakate <ajay.jakate@gmail.com>
2022-12-18 17:22:21 -0600

    it actually works

--- run.clj
+++ run.clj
@@ -1,11 +1,28 @@
-(require '[babashka.process :refer [sh]])
+(require '[babashka.process :refer [sh shell]]
+         '[clojure.string :as str])
 
 (def input-file "inputs/test_new.mov")
+(def input-playlist "inputs/test_movie.m3u")
+(def output-file "outputs/out.mp4")
 
-(def inc-fact 2.0)
+(def inc-fact 1.14)
 
 (defn print-round [num, prec]
   (apply str (take prec (str num))))
+
+(defn convert-time [seconds]
+  (let [split-time (str/split seconds #"\.")
+        raw-seconds (first split-time)
+        raw-millis (second split-time)
+        raw-seconds-int (Integer/parseInt raw-seconds)
+        full-hours (quot raw-seconds-int 3600)
+        remaining-minutes (rem raw-seconds-int 3600)
+        full-minutes (quot remaining-minutes 60)
+        full-seconds (rem remaining-minutes 60)]
+    (str
+     (format "%02d" full-hours) "\\:"
+     (format "%02d" full-minutes) "\\:"
+     (format "%02d" full-seconds) "." raw-millis)))
 
 (defn get-speeds-for-index [idx]
   (let [raw-speed (reduce * (repeat (+ 1 idx) (float inc-fact)))
@@ -13,48 +30,39 @@
     {:audio (print-round raw-speed 6)
      :video (print-round inverse 6)}))
 
-(def timestamps
-  ["00:08.500"
-   "00:13.750"
-   "00:17.500"])
+(def stamps-file (slurp input-playlist))
+
+(def stamps-match (re-seq #"(?s)time=(\d+.\d+)}" stamps-file))
+
+(def timestamps (map second stamps-match))
 
 (def num-segments (+ (count timestamps) 1))
 
-(defn safely [stringg]
-  (let [chary (seq (char-array stringg))]
-    (reduce (fn [state item]
-              (if (= item \:)
-                (str state \\ \:)
-                (str state item)))
-            ""
-            chary)))
-
-(def firstone (first timestamps))
-
-(def command-start (str "ffmpeg -y -i " input-file " -filter_complex "))
+(def command-start (str "ffmpeg -y -i " input-file "  -filter_complex "))
 
 (def first-segment
-  (str "[0:v]trim=" firstone
-       ",setpts=(PTS-STARTPTS)[sv];[0:a]atrim="
-       firstone
-       ",asetpts=(PTS-STARTPTS)[sa];"))
+  (let [first-stamp (convert-time (first timestamps))]
+    (str "[0:v]trim=end='" first-stamp
+         "',setpts=(PTS-STARTPTS)[sv];[0:a]atrim=end='"
+         first-stamp
+         "',asetpts=(PTS-STARTPTS)[sa];")))
 
 (def iterator (map vector timestamps (rest timestamps)))
 
 (def chunkss
   (map-indexed
    (fn [idx stamps]
-     (let [start (safely (first stamps))
-           end (safely (second stamps))
+     (let [start (convert-time (first stamps))
+           end (convert-time (second stamps))
            speeds (get-speeds-for-index idx)]
        (str "[0:v]trim=start='" start "':end='" end "',setpts=" (:video speeds) "*(PTS-STARTPTS)[v" idx "];"
             "[0:a]atrim=start='" start "':end='" end "',asetpts=(PTS-STARTPTS),atempo=" (:audio speeds) "[a" idx "];")))
    iterator))
 
 (def last-segment
-  (let [speeds (get-speeds-for-index (- num-segments 1))] 
-    (str "[0:v]trim=start='" (safely (last timestamps)) "',setpts=" (:video speeds) "*(PTS-STARTPTS)[ev];"
-         "[0:a]atrim=start='" (safely (last timestamps)) "',asetpts=(PTS-STARTPTS),atempo=" (:audio speeds) "[ea];")))
+  (let [speeds (get-speeds-for-index (- num-segments 2))]
+    (str "[0:v]trim=start='" (convert-time (last timestamps)) "',setpts=" (:video speeds) "*(PTS-STARTPTS)[ev];"
+         "[0:a]atrim=start='" (convert-time (last timestamps)) "',asetpts=(PTS-STARTPTS),atempo=" (:audio speeds) "[ea];")))
 
 (def last-line
   (str "[sv][sa]"
@@ -62,10 +70,10 @@
        "[ev][ea]concat=n=" num-segments ":v=1:a=1"))
 
 (def final
-  (str command-start "\"" first-segment (apply str chunkss) last-segment last-line "\"" " outputs/out.mov"))
+  (str "set -x\n\n" command-start "\"" first-segment (apply str chunkss) last-segment last-line "\" " output-file))
 
 (spit "run.sh" final)
 
-(sh "sh run.sh")
+(shell "sh run.sh")
 
 (sh "rm -rf run.sh")
